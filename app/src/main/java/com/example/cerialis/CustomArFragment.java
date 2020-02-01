469d276b7601ec10deeb41aead8cc8d07591b360
mohamed belkhir <momoh.belkhir@gmail.com>
2020-02-01 20:51:36 +0100

    take photo

--- app/src/main/java/com/example/cerialis/CustomArFragment.java
+++ app/src/main/java/com/example/cerialis/CustomArFragment.java
@@ -1,5 +1,7 @@
 package com.example.cerialis;
 
+import android.Manifest;
+
 import com.google.ar.core.Config;
 import com.google.ar.core.Session;
 import com.google.ar.sceneform.ux.ArFragment;
@@ -17,5 +19,18 @@ public class CustomArFragment extends ArFragment {
         ((MainActivity) getActivity()).stupDatabase(config,session);
 
         return config;
+    }
+
+
+    @Override
+    public String[] getAdditionalPermissions() {
+        String[] additionalPermissions = super.getAdditionalPermissions();
+        int permissionLength = additionalPermissions != null ? additionalPermissions.length : 0;
+        String[] permissions = new String[permissionLength + 1];
+        permissions[0] = Manifest.permission.WRITE_EXTERNAL_STORAGE;
+        if (permissionLength > 0) {
+            System.arraycopy(additionalPermissions, 0, permissions, 1, additionalPermissions.length);
+        }
+        return permissions;
     }
 }
--- app/src/main/java/com/example/cerialis/MainActivity.java
+++ app/src/main/java/com/example/cerialis/MainActivity.java
@@ -1,12 +1,25 @@
 package com.example.cerialis;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.media.Image;
+
 import android.net.Uri;
-import android.support.v7.app.AlertDialog;
+
+import android.os.Environment;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.PixelCopy;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.ar.core.Anchor;
 import com.google.ar.core.AugmentedImage;
@@ -16,15 +29,24 @@ import com.google.ar.core.Frame;
 import com.google.ar.core.Session;
 import com.google.ar.core.TrackingState;
 import com.google.ar.sceneform.AnchorNode;
+import com.google.ar.sceneform.ArSceneView;
 import com.google.ar.sceneform.FrameTime;
 import com.google.ar.sceneform.Scene;
 import com.google.ar.sceneform.rendering.ModelRenderable;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.text.SimpleDateFormat;
 import java.util.Collection;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity implements Scene.OnUpdateListener{
 
     private CustomArFragment arFragment;
+    Button capture;
 
 
     @Override
@@ -32,11 +54,98 @@ public class MainActivity extends AppCompatActivity implements Scene.OnUpdateLis
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+       
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_DENIED){
+            ActivityCompat.requestPermissions(this,
+                    new String[]{ Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+        }
+
         arFragment = (CustomArFragment) getSupportFragmentManager().findFragmentById(R.id.fragment);
         arFragment.getArSceneView().getScene().addOnUpdateListener(this);
 
+        capture = (Button) findViewById(R.id.capture);
+
+        capture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                 takePhoto();
+            }
+        });
 
     }
+
+    private String generateFilename() {
+        String date =
+                new SimpleDateFormat("yyyyMMddHHmmss", java.util.Locale.getDefault()).format(new Date());
+        return Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES) + File.separator  + date + "_screenshot.jpg";
+    }
+
+    private void saveBitmapToDisk(Bitmap bitmap, String filename) throws IOException {
+
+
+        try  {
+            FileOutputStream outputStream = new FileOutputStream(filename);
+            ByteArrayOutputStream outputData = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputData);
+            outputData.writeTo(outputStream);
+            outputStream.flush();
+            outputStream.close();
+
+        } catch (IOException ex) {
+            Toast.makeText(this, "Save filed : "+ ex, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void takePhoto() {
+        final String filename = generateFilename();
+        ArSceneView view = arFragment.getArSceneView();
+
+        // Create a bitmap the size of the scene view.
+        final Bitmap bitmap = Bitmap.createBitmap(view.getWidth(), view.getHeight(),
+                Bitmap.Config.ARGB_8888);
+
+        // Create a handler thread to offload the processing of the image.
+        final HandlerThread handlerThread = new HandlerThread("PixelCopier");
+        handlerThread.start();
+        // Make the request to copy.
+        PixelCopy.request(view, bitmap, (copyResult) -> {
+            if (copyResult == PixelCopy.SUCCESS) {
+                try {
+                    saveBitmapToDisk(bitmap, filename);
+                } catch (IOException e) {
+                    Toast toast = Toast.makeText(MainActivity.this, e.toString(),
+                            Toast.LENGTH_LONG);
+                    toast.show();
+                    return;
+                }
+                Snackbar snackbar = Snackbar.make(findViewById(android.R.id.content),
+                        "Photo saved", Snackbar.LENGTH_LONG);
+                snackbar.setAction("Open in Photos", v -> {
+                    File photoFile = new File(filename);
+
+                    Uri photoURI = FileProvider.getUriForFile(MainActivity.this,
+                            MainActivity.this.getPackageName() + ".ar.codelab.name.provider",
+                            photoFile);
+                    Intent intent = new Intent(Intent.ACTION_VIEW, photoURI);
+                    intent.setDataAndType(photoURI, "image/*");
+                    intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                    startActivity(intent);
+
+                });
+                snackbar.show();
+            } else {
+                Toast toast = Toast.makeText(MainActivity.this,
+                        "Failed to copyPixels: " + copyResult, Toast.LENGTH_LONG);
+                toast.show();
+            }
+            handlerThread.quitSafely();
+        }, new Handler(handlerThread.getLooper()));
+    }
+
+
 
     public void stupDatabase (Config config, Session session){
 
